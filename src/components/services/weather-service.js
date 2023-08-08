c3d87a084f2ca234498ed84ea57a4716eb20b9d4
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-08-08 18:09:17 +0200

    Add function to sort through data

--- src/components/services/weather-service.js
+++ src/components/services/weather-service.js
@@ -1,10 +1,24 @@
 const API_KEY = '0d9d028bd7cf44ec9fe94100231107';
 
+const sortWeatherData = async function sortWeatherData(weatherData) {
+  const sortedData = {
+    location: weatherData.location.name,
+    windKph: weatherData.current.wind_kph,
+    windMph: weatherData.current.wind_mph,
+    humidity: weatherData.current.humidity,
+    tempC: weatherData.current.temp_c,
+    tempF: weatherData.current.temp_f,
+  };
+
+  return sortedData;
+};
+
 const getWeather = async function getWeather(location) {
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`);
   const responseData = await response.json();
+  const weatherData = sortWeatherData(responseData);
 
-  return responseData;
+  return weatherData;
 };
 
 export default getWeather;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './reset.css';
 import './style.css';
+import getWeather from './components/services/weather-service';
+
+getWeather('london').then((res) => { console.log(res); });
