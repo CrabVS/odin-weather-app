2378a7e1ff0f8ce24893e371417145134c52e476
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-08-08 19:18:29 +0200

    Add info display updater

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,28 @@
 import getWeather from '../services/weather-service';
 import './home.css';
 
+const getInfoElements = function getInfoElements() {
+  const infoElements = {
+    conditionIcon: document.getElementById('info-icon'),
+    condition: document.getElementById('info-condition'),
+    location: document.getElementById('info-location'),
+    wind: document.getElementById('info-wind'),
+    humidity: document.getElementById('info-humidity'),
+  };
+
+  return infoElements;
+};
+
+const updatePage = function updatePageInformation(weatherData) {
+  const infoElements = getInfoElements();
+
+  infoElements.conditionIcon.src = weatherData.condition.icon;
+  infoElements.condition.textContent = weatherData.condition.text;
+  infoElements.location.textContent = `${weatherData.location} ${weatherData.tempC}°C`;
+  infoElements.wind.textContent = `Wind: ${weatherData.windKph}`;
+  infoElements.humidity.textContent = `Humidity: ${weatherData.humidity}`;
+};
+
 const initializeForm = function initializeForm() {
   const searchEl = document.getElementById('location-search');
   searchEl.addEventListener('keydown', (event) => {
@@ -8,7 +30,7 @@ const initializeForm = function initializeForm() {
       event.preventDefault();
       const location = searchEl.value;
       getWeather(location).then((res) => {
-        console.log(res);
+        updatePage(res);
       });
     }
   });
