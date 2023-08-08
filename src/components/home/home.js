bdccbc81c626cc2815daf81b38468f9a5504cbe1
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-08-08 21:43:48 +0200

    Add content clearer and disabled input

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,6 +5,29 @@ let currentLocation;
 let currentDeg = 'C';
 let currentSpeed = 'KpH';
 
+const disableButtons = function disableButtons() {
+  const meassureButtons = document.querySelectorAll('#data-button-group .btn');
+
+  meassureButtons[0].disabled = true;
+  meassureButtons[1].disabled = true;
+};
+
+const disableElements = function disableElements() {
+  const searchEl = document.getElementById('location-search');
+
+  searchEl.disabled = true;
+  disableButtons();
+};
+
+const enableElements = function enableElements() {
+  const searchEl = document.getElementById('location-search');
+  const meassureButtons = document.querySelectorAll('#data-button-group .btn');
+
+  searchEl.disabled = false;
+  meassureButtons[0].disabled = false;
+  meassureButtons[1].disabled = false;
+};
+
 const getInfoElements = function getInfoElements() {
   const infoElements = {
     conditionIcon: document.getElementById('info-icon'),
@@ -25,20 +48,38 @@ const getSpeed = function getSpeed(weatherData) {
   return weatherData[`wind${currentSpeed}`];
 };
 
+const clearDisplay = function clearDisplay() {
+  const infoElements = getInfoElements();
+
+  infoElements.conditionIcon.src = '';
+  infoElements.conditionIcon.alt = '';
+  infoElements.condition.textContent = '';
+  infoElements.location.textContent = '';
+  infoElements.wind.textContent = '';
+  infoElements.humidity.textContent = '';
+};
+
 const updatePage = function updatePageInformation(weatherData) {
   const infoElements = getInfoElements();
 
   infoElements.conditionIcon.src = weatherData.condition.icon;
+  infoElements.conditionIcon.alt = 'weather icon';
   infoElements.condition.textContent = weatherData.condition.text;
   infoElements.location.textContent = `${weatherData.location} ${getDegrees(weatherData)}°${currentDeg}`;
   infoElements.wind.textContent = `Wind: ${getSpeed(weatherData)} ${currentSpeed}`;
   infoElements.humidity.textContent = `Humidity: ${weatherData.humidity}`;
 };
 
+const showLoading = function showLoading() {
+  clearDisplay();
+};
+
 const fetchData = async function fetchData(location) {
+  showLoading();
   const weatherData = await getWeather(location);
   updatePage(weatherData);
   currentLocation = weatherData.location;
+  enableElements();
 };
 
 const initializeForm = function initializeForm() {
@@ -46,7 +87,10 @@ const initializeForm = function initializeForm() {
   searchEl.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
+      disableElements();
+
       const location = searchEl.value;
+      searchEl.value = '';
       fetchData(location);
     }
   });
@@ -60,14 +104,16 @@ const initializeButtons = function initializeButtons() {
     meassureButtons[0].textContent = `To °${currentDeg}`;
 
     currentDeg = altDeg;
+    disableElements();
     fetchData(currentLocation);
   });
 
   meassureButtons[1].addEventListener('click', () => {
     const altSpeed = currentSpeed === 'KpH' ? 'MpH' : 'KpH';
-    meassureButtons[1].textContent = `To ${altSpeed}`;
+    meassureButtons[1].textContent = `To ${currentSpeed}`;
 
     currentSpeed = altSpeed;
+    disableElements();
     fetchData(currentLocation);
   });
 };
@@ -75,6 +121,8 @@ const initializeButtons = function initializeButtons() {
 const initializeHome = function initializeHome() {
   initializeForm();
   initializeButtons();
+  clearDisplay();
+  disableButtons();
 };
 
 export default initializeHome;
